841adf306fe4e09f696a553c3ea653a1b9de2d2c
rhit-werdmabl <131706831+rhit-werdmabl@users.noreply.github.com>
2025-06-03 19:58:50 +0900

    Progress Save
    Working on dissapearing ammo

--- Assets/ProjectScripts/WeaponScripts/DestroyProjectile.cs
+++ Assets/ProjectScripts/WeaponScripts/DestroyProjectile.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Events;
 using UnityEngine;
+using System;
 
 public class DestroyProjectile : MonoBehaviour
 {
 
-    public void Collision()
-    {
-        Destroy(gameObject);
-    }
+    [Serializable] public class CollisionEvent : UnityEvent<Collision> { }
 
-    // Update is called once per frame
-    void Update()
+    private int collisions = 0;
+
+    private void OnCollisionExit(Collision collision)
     {
-        
+        collisions += 1;
+        if(collisions >= 2){
+            Destroy(gameObject);
+        }
     }
 }
